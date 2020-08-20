471f84a56c4bd62240549b7a81c04acdde24923e
thiagoRocha <thiago.rocha@to-brasil.com>
2020-08-19 23:21:10 -0300

    melhorando regras do checkbox

--- src/views/TableAutoScalling.js
+++ src/views/TableAutoScalling.js
@@ -15,55 +15,47 @@ export default class TableAutoScaling extends React.Component{
     
   }
 
-
     handleChecked = event => {
-      //this.setState({Check: true});
+      event.preventDefault();
       axios.put(`http://localhost:4000/dev/asgroups/openspot/enable`,
       {  
-          "auto-scaling-group-name": "openspot",
+          "auto-scaling-group-name": event.target.getAttribute('name'),
           "openspot": event.target.value
       }).then(res =>{
+        if(!this.state.isChecked === true){
+          alert("Tag Spot vinculada com sucesso !")
+          //window.location.reload(false)
+        }else{
+          alert("Tag Spot desvinculada !")
+          //window.location.reload(false)
+        }              
         const tag = JSON.parse(res.config.data)
         this.setState({ 
           isChecked:  !this.state.isChecked,
+          tag: tag
           
          });
-          console.log(tag)            
+          console.log(tag)
+          console.log(this.state.isChecked , 'do put')            
       });
     }
 
-
     componentDidMount(){      
         axios.get(`http://localhost:4000/dev/asgroups`).then(res =>{
-          const autoGroups = JSON.parse(res.data)           
+          const autoGroups = JSON.parse(res.data)
+          console.log(autoGroups)
           this.setState({ 
             Autoscaling: autoGroups.AutoScalingGroups,
             Enable: autoGroups.AutoScalingGroups[0].Tags[0].Value,
             
            });
-           console.log(autoGroups.AutoScalingGroups[0].Tags[0].Value)
         });
 
         
     }
 
-
-
     render(){   
-    
-      var attrs = this.state.isChecked
-      var isvalue = ""
-      console.log(isvalue)
-      console.log(this.state.isChecked)
-      if (this.state.isChecked == false) {
-        
-        attrs = false
-        isvalue = "disabled"
-      } else {
-        attrs = true
-        isvalue = "enabled"
-      }
-
+      
          return(
             <div class="row" >
             <div class="col">
@@ -76,7 +68,6 @@ export default class TableAutoScaling extends React.Component{
                   <table class="table mb-7"  >
                     <thead class="bg-light">
                       <tr>
-                        
                         <th scope="col" class="border-0">Name</th>
                         <th scope="col" class="border-0">Desired Capacity</th>
                         <th scope="col" class="border-0">MaxSize</th>
@@ -87,25 +78,27 @@ export default class TableAutoScaling extends React.Component{
                     </thead>
                     <tbody>
                     { this.state.Autoscaling.map((Autoscaling) =>
-                        <tr>
-                        
-                          <td><a href="teste">{Autoscaling.AutoScalingGroupName}</a></td>
+                        <tr> 
+                          <td>
+                          <a href="teste" >{Autoscaling.AutoScalingGroupName}</a>
+                          </td>
                           <td>{Autoscaling.DesiredCapacity}</td>   
                           <td>{Autoscaling.MaxSize}</td>                
                           <td>{Autoscaling.MinSize}</td>
                           <td>{Autoscaling.AvailabilityZones}</td>
-                    <td>{isvalue}</td>
+                          <td>{Autoscaling.Tags[0].Value}</td>
                           <td>
                             <div class="custom-control custom-checkbox mb-1" >
                             <input 
                             type="checkbox" 
                             class="custom-control-input"
-                            id="formsCheckboxDefault" 
+                            id={Autoscaling.AutoScalingGroupName} 
                             onClick={ this.handleChecked }
-                            value={isvalue}
-                            defaultChecked={attrs}
+                            name={Autoscaling.AutoScalingGroupName}  
+                            value={!this.state.isChecked == true ? "enabled" : "disabled" }
+                            defaultChecked={Autoscaling.Tags[0].Value == "enabled" ? true : false }
                              ></input>
-                            <label class="custom-control-label" for="formsCheckboxDefault"></label>
+                            <label class="custom-control-label" for={Autoscaling.AutoScalingGroupName}></label>
                           </div>
                         </td>  
                         </tr>
